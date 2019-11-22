4d9b624e67ded8e117d822d8ba982192ee634bb2
yiannigiannaris <yiannig@mit.edu>
2019-11-22 01:54:00 -0500

    fixed create_send_mouse and create_send_rectangle funciton features

--- kernel/virtio_gpu.c
+++ kernel/virtio_gpu.c
@@ -675,10 +675,11 @@ virtio_gpu_intr(){
   release(&gpu.lock);
 }
 
-void create_send_rectangle(int n){
+void create_send_rectangle(){
   update_frame_buffer((uint64)gpu.framebuffer);
 }
-void create_send_mouse(int n)
+
+void create_send_mouse()
 {
   update_cursor(20, 20, (uint64)get_cursor_frame_buf());
 }
